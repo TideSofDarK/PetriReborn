9bd7e047278427fb46ad1dce91bf1ccf5f4a936a
TideSofDarK <tidesmain@gmail.com>
2015-07-29 12:43:00 +0500

    Корректное время ожидания постройки лабы

--- game/dota_addons/petri_reborn/scripts/vscripts/units/lab.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/units/lab.lua
@@ -2,7 +2,7 @@ function Spawn( keys )
 	StartAnimation(thisEntity, {duration=-1, activity=ACT_DOTA_IDLE , rate=2.5})
 	thisEntity:SetAngles(0, -90, 0)
 
-	Timers:CreateTimer(5.1, function()
+	Timers:CreateTimer(tonumber(GameMode.AbilityKVs["build_petri_lab"]["BuildTime"]) + 0.1, function()
 		local pID = thisEntity:GetPlayerOwnerID()
 		if pID ~= -1 then
 			local level = GetUpgradeLevelForPlayer("petri_upgrade_concrete", pID)
