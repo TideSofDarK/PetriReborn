4d418fe16f36b5ad4297fc31c5c29a7966471307
TideSofDarK <tidesmain@gmail.com>
2015-08-15 02:02:59 +0500

    Cleanup

--- game/dota_addons/petri_reborn/scripts/vscripts/units/creep.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/units/creep.lua
@@ -1,5 +1,4 @@
 function Spawn( keys )
-	print("asdsadsa")
 	thisEntity:AddAbility("petri_building")
 	thisEntity:FindAbilityByName("petri_building"):ApplyDataDrivenModifier(thisEntity, thisEntity, "modifier_disabled_invulnerable", {})
 	thisEntity:SetAttackCapability(0)
