6a1613441be8544f1f970e34144e4c70410c237e
TideSofDarK <tidesmain@gmail.com>
2015-08-17 08:39:31 +0500

    Exploration tree's explore now is visible to all players

--- game/dota_addons/petri_reborn/scripts/vscripts/units/exploration_tree.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/units/exploration_tree.lua
@@ -8,13 +8,9 @@ function Explore(keys)
 	local particleName = "particles/items_fx/dust_of_appearance.vpcf"
 
 	-- Particle for team
-	for _, v in pairs( allHeroes ) do
-		if v:GetPlayerID() and v:GetTeam() == caster:GetTeam() then
-			local fxIndex = ParticleManager:CreateParticleForPlayer( particleName, PATTACH_WORLDORIGIN, v, PlayerResource:GetPlayer( v:GetPlayerID() ) )
-			ParticleManager:SetParticleControl( fxIndex, 0, target )
-			ParticleManager:SetParticleControl( fxIndex, 1, Vector(1400,0,1400) )
-		end
-	end
+	local fxIndex = ParticleManager:CreateParticle( particleName, PATTACH_WORLDORIGIN, GameMode.assignedPlayerHeroes[caster:GetPlayerOwnerID()])
+	ParticleManager:SetParticleControl( fxIndex, 0, target )
+	ParticleManager:SetParticleControl( fxIndex, 1, Vector(1400,0,1400) )
 
 	local dummy = CreateUnitByName("petri_dummy_1400vision", target, false, caster, caster, caster:GetTeamNumber())
 	InitAbilities(dummy)
