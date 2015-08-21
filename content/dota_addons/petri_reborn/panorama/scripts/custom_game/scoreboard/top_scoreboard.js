ed8b03d2fcb559423fc1a318df32d7e9b2693bea
K1llMan <k1llmanws@yandex.ru>
2015-08-21 16:11:32 +0300

    У top scoreboard уменьшено время обновления

--- content/dota_addons/petri_reborn/panorama/scripts/custom_game/scoreboard/top_scoreboard.js
+++ content/dota_addons/petri_reborn/panorama/scripts/custom_game/scoreboard/top_scoreboard.js
@@ -11,7 +11,7 @@ function UpdateScoreboard()
 {
 	ScoreboardUpdater_SetScoreboardActive( g_ScoreboardHandle, true );
 
- 	$.Schedule( 0.8, UpdateScoreboard );
+ 	$.Schedule( 0.5, UpdateScoreboard );
 }
 
 (function()
