c5020bb0132627e8fa6e0d090c12e30f91e93ca0
TideSofDarK <tidesmain@gmail.com>
2015-09-22 19:21:15 +0500

    Slightly reworked lottery

--- game/dota_addons/petri_reborn/scripts/vscripts/lottery.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/lottery.lua
@@ -54,10 +54,11 @@ function SelectWinner()
 		local pID = tonumber(k)
 		if v == winner then
 			GameMode.assignedPlayerHeroes[pID]:ModifyGold(GameMode.CURRENT_BANK/2, false, 0)
-			Notifications:Top(pID, {text="#win_lottery_1", 					duration=9, continue=false, style={color="white", ["font-size"]="45px"}})
-			Notifications:Top(pID, {text=tostring(math.floor(GameMode.CURRENT_BANK/2)), duration=9, continue=true, style={color="white", ["font-size"]="45px"}})
+			Notifications:Top(pID, {text="#win_lottery_1", duration=9, continue=false, style={color="white", ["font-size"]="45px"}})
+			Notifications:Top(pID, {text=tostring(math.floor(GameMode.CURRENT_BANK/2)).."$", duration=9, continue=true, style={color="white", ["font-size"]="45px"}})
 		else
-
+			Notifications:Top(pID, {text="#lose_lottery_1", duration=4, continue=false, style={color="white", ["font-size"]="45px"}})
+			Notifications:Top(pID, {text=tostring(math.floor(GameMode.CURRENT_BANK/10)).."$", duration=9, continue=true, style={color="white", ["font-size"]="45px"}})
 		end
 	end
 
