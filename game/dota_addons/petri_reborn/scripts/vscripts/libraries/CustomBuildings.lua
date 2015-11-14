3e5a07dc90fbc57f24358780c9d11bd510b1cb93
TideSofDarK <tidesmain@gmail.com>
2015-11-14 22:52:57 +0500

    Some optimizations to custom_buildings.kv parsing

--- game/dota_addons/petri_reborn/scripts/vscripts/libraries/CustomBuildings.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/libraries/CustomBuildings.lua
@@ -1,61 +1,58 @@
 function SetCustomBuildingModel(building, steamID, level)
-	ParseCustomBuldingKVs("default", building, steamID, level)
+	local key = tostring(steamID)
+	local name = building:GetUnitName()
+	if level then name = name.."_"..level end
 
-	if not GameMode.CustomBuildingsKVs[tostring(steamID)] then return nil end
+	local defaultScale = ParseCustomBuldingKVs(GameMode.CustomBuildingsKVs["default"][name], building, level)
 
-	return ParseCustomBuldingKVs(tostring(steamID), building, steamID, level)
+	if not GameMode.CustomBuildingsKVs[key][name] then return defaultScale end
+
+	return ParseCustomBuldingKVs(GameMode.CustomBuildingsKVs[key][name], building, level)
 end
 
-function ParseCustomBuldingKVs(key, building, steamID, level)
-	for k,v in pairs(GameMode.CustomBuildingsKVs[key]) do
-		local name = building:GetUnitName()
-		if level then name = name.."_"..level end
+function ParseCustomBuldingKVs(entry, building, level)
+	local v = entry
+	if not v then return 1.0 end
 
-		if k == name then
-			if type(v)=="table" then
-				local scale = building:GetModelScale()
+	local scale = building:GetModelScale()
 
-				local yaw = building:GetAngles()[2]
+	local yaw = building:GetAngles()[2]
 
-				for k2,v2 in pairs(v) do
-			        if v2 == "scale" then
-			        	scale = tonumber(k2)
-			        end
-			    end
+	if type(v)=="table" then
+		for k2,v2 in pairs(v) do
+	        if v2 == "scale" then
+	        	scale = tonumber(k2)
+	        end
+	    end
 
-			    for k2,v2 in pairs(v) do
-			        if v2 == "yaw" then
-			        	yaw = tonumber(k2)
-			        end
-			    end
+	    for k2,v2 in pairs(v) do
+	        if v2 == "yaw" then
+	        	yaw = tonumber(k2)
+	        end
+	    end
 
-			    if building.attachedModels then
-			    	for i2,v2 in ipairs(building.attachedModels) do
-			    		if v2:IsNull() == false then v2:RemoveSelf() end
-					end
-			    end
-
-				for k2,v2 in pairs(v) do
-			        if v2 == "model" then
-			        	UpdateModel(building, k2, scale)
-			        end
-			    end
-
-			    for k2,v2 in pairs(v) do
-			        if v2 ~= "model" then
-			        	Attachments:AttachProp(building, v2, k2, nil)
-			        end
-			    end
-
-			    building:SetAngles(building:GetAngles()[1], yaw, building:GetAngles()[3])
-
-			    return scale
-			else 
-				UpdateModel(building, v, building:GetModelScale())
-
-				return building:GetModelScale()
+	    if building.attachedModels then
+	    	for i2,v2 in ipairs(building.attachedModels) do
+	    		if v2:IsNull() == false then v2:RemoveSelf() end
 			end
-			break
-		end
+	    end
+
+		for k2,v2 in pairs(v) do
+	        if v2 == "model" then
+	        	UpdateModel(building, k2, scale)
+	        end
+	    end
+
+	    for k2,v2 in pairs(v) do
+	        if v2 ~= "model" then
+	        	Attachments:AttachProp(building, v2, k2, nil)
+	        end
+	    end
+
+	    building:SetAngles(building:GetAngles()[1], yaw, building:GetAngles()[3])
+	else 
+		UpdateModel(building, v, building:GetModelScale())
 	end
+
+	return scale
 end
