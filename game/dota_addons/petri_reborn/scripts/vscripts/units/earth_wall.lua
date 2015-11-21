f214716c6d9426ede9b8d1b2eb497a9a96c12715
TideSofDarK <tidesmain@gmail.com>
2015-11-22 02:54:18 +0500

    Fixed earth wall blockers

--- game/dota_addons/petri_reborn/scripts/vscripts/units/earth_wall.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/units/earth_wall.lua
@@ -4,12 +4,13 @@ function UpgradeToEarthWall( keys )
 	local pos = caster:GetAbsOrigin()
 	local player = caster:GetPlayerOwner()
 
-	caster:RemoveBuilding( true )
+	local gridNavBlockers = caster.blockers
+
 	UTIL_Remove(caster)
 
-	local wall = BuildingHelper:PlaceBuilding(player, "npc_petri_earth_wall", pos, true, true, 2)
+	local wall = BuildingHelper:PlaceBuilding(player, "npc_petri_earth_wall", pos, true, false, 2)
+	wall.blockers = gridNavBlockers
 
-	--UpdateModel(wall, "models/items/earth_spirit/demon_stone_summon/demon_stone_summon.vmdl", 0.8)
 	InitAbilities(wall)
 
 	Timers:CreateTimer(0.03, function (  )
