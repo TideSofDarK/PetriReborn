1653162a077f20600b80cff4f1b81f9bbf0c3a21
K1llMan <k1llmanws@yandex.ru>
2016-01-30 01:00:37 +0300

    Split GridNav and BuildingHelper modules

--- content/dota_addons/petri_reborn/panorama/scripts/custom_game/building_helper/gnv.js
+++ content/dota_addons/petri_reborn/panorama/scripts/custom_game/building_helper/gnv.js
@@ -1,9 +1,33 @@
 'use strict';
+// Store received data into panel data
+var GNVPanel = $.GetContextPanel();
+if (!GNVPanel.loaded)
+{
+    GNVPanel.Grid = [];
+    GNVPanel.LastQueueNum = -1;
+    GNVPanel.XMin = 0;
+    GNVPanel.XMax = 0;
+    GNVPanel.YMin = 0;
+    GNVPanel.YMax = 0;
+    GNVPanel.Resolution = [];
+
+    GNVPanel.loaded = true;
+}
 
 var state = 'disabled';
-var size = 0;
-var particle;
-var gridParticles;
+
+//-----------------------------------------------------------------------------
+//                         Main function
+//-----------------------------------------------------------------------------
+(function () {
+    GameEvents.SendCustomGameEventToServer( "gnv_request", { "Layers" : { } } );
+
+    CustomNetTables.SubscribeNetTableListener( "LayersQueue", LayerChanged );
+    GameEvents.Subscribe( "gnv", GNV);
+
+	GNVPanel.CreateParticles = CreateParticles;
+    GNVPanel.DestroyParticles = DestroyParticles;
+})();
 
 //-----------------------------------------------------------------------------
 //                          Visible grid vars
@@ -25,12 +49,9 @@ var GridMode = {
     OnlyFree: 5
 }
 
-var gridMode = GridMode.None;
-
 // Position for hided particles
 var outPos = [-1000, -1000, -1000];
 
-var mouseRadius = 250;
 var altPressed = false;
 
 // Position to detect if need remap grid
@@ -42,25 +63,18 @@ var screenStepSize = 16;
 var topInsetOverride = 30;
 var bottomInsetOverride = 100;
 
-// Ghost Building Preferences
-var GRID_ALPHA = 30 // Defines the transparency of the ghost squares
-var VISIBLE_GRID_ALPHA = 255 // Defines the transparency of the ghost squares
+//-----------------------------------------------------------------------------
+//                         Grid config
+//-----------------------------------------------------------------------------
+var GridConfig = {};
+GridConfig.EntityGridAlpha = 30;
+GridConfig.VisibleGridAlpha = 255;
+GridConfig.FieldRadius = 250;
+GridConfig.GridMode = GridMode.None;
 
-
-// Store received data into panel data
-var BHPanel = $.GetContextPanel();
-if (!BHPanel.loaded)
-{
-    BHPanel.Grid = [];
-    BHPanel.LastQueueNum = -1;
-    BHPanel.XMin = 0;
-    BHPanel.XMax = 0;
-    BHPanel.YMin = 0;
-    BHPanel.YMax = 0;
-    BHPanel.Resolution = [];
-
-    BHPanel.loaded = true;
-}
+//-----------------------------------------------------------------------------
+//                         Visible grid
+//-----------------------------------------------------------------------------
 
 function GetQuadColor( quadStatus )
 {
@@ -102,9 +116,6 @@ function GetQuadStatus( pos )
     return QuadStatus.Free;
 }
 
-//-----------------------------------------------------------------------------
-//                    Visible grid
-//-----------------------------------------------------------------------------
 function GetScreenResolution()
 {
     // Full screen viewport
@@ -160,16 +171,16 @@ function CreateVisibleGridParticle()
     DestroyVisibleGridParticles();
 
     visibleGrid = [];
-    for (var x = 0; x < BHPanel.Resolution[0]; x += screenStepSize)
-        for (var y = topInsetOverride; y < BHPanel.Resolution[1] - bottomInsetOverride; y += screenStepSize)
+    for (var x = 0; x < GNVPanel.Resolution[0]; x += screenStepSize)
+        for (var y = topInsetOverride; y < GNVPanel.Resolution[1] - bottomInsetOverride; y += screenStepSize)
         {
             var pos = ScreenToSnapWorldPos( x, y );
 
             if (!pos)
                 continue;
 
-            var gridX = WorldToGridPosX(pos[0]) - BHPanel.XMin;
-            var gridY = WorldToGridPosY(pos[1]) - BHPanel.YMin;
+            var gridX = WorldToGridPosX(pos[0]) - GNVPanel.XMin;
+            var gridY = WorldToGridPosY(pos[1]) - GNVPanel.YMin;
 
             if (!visibleGrid[gridX])
                 visibleGrid[gridX] = [];
@@ -183,7 +194,7 @@ function CreateVisibleGridParticle()
             Particles.SetParticleControl(gridParticle, 0, outPos);
             Particles.SetParticleControl(gridParticle, 1, [32,0,0]);
             Particles.SetParticleControl(gridParticle, 2, GetQuadColor(status));
-            Particles.SetParticleControl(gridParticle, 3, [VISIBLE_GRID_ALPHA,0,0]);
+            Particles.SetParticleControl(gridParticle, 3, [GridConfig.VisibleGridAlpha,0,0]);
 
             visibleGrid[gridX][gridY] = [];
             visibleGrid[gridX][gridY]["Particle"] = gridParticle;
@@ -239,7 +250,7 @@ function GetScreenCenterPos()
     if (state != 'active')
         return;
 
-    var curPos = ScreenToSnapWorldPos( BHPanel.Resolution[0] / 2, BHPanel.Resolution[1] / 2 );
+    var curPos = ScreenToSnapWorldPos( GNVPanel.Resolution[0] / 2, GNVPanel.Resolution[1] / 2 );
 
     if (!IsPointInRange(curPos, screenCenterPos, 64))
     {
@@ -251,7 +262,6 @@ function GetScreenCenterPos()
 function SnapMousePosToWorld()
 {
     var mousePos = GameUI.GetCursorPosition();
-
     var gamePos = GameUI.GetScreenWorldPosition( mousePos[0], mousePos[1] );
 
     if (!gamePos)
@@ -261,9 +271,9 @@ function SnapMousePosToWorld()
 
     var offset = 0;
 
-    if (gridMode == GridMode.AroundBuildingQuad)
+    if (GridConfig.GridMode == GridMode.AroundBuildingQuad)
         offset = 16;
-    if (gridMode == GridMode.AroundBuildingCircle)
+    if (GridConfig.GridMode == GridMode.AroundBuildingCircle)
         offset = 32;
 
     var xSign = gamePos[0] < snapGamePos[0] ? -1 : 1;
@@ -288,19 +298,19 @@ function UpdateVisibleGrid()
             var status = GetQuadStatus(visibleGrid[x][y]["Position"]);
             var pos = visibleGrid[x][y]["Position"];
 
-            switch(gridMode)
+            switch(GridConfig.GridMode)
             {
                 case GridMode.None:
                     pos = outPos;
                     break;
 
                 case GridMode.AroundBuildingQuad:
-                    if (!IsPointInQuad( gamePos, pos, mouseRadius * 2 ))
+                    if (!IsPointInQuad( gamePos, pos, GridConfig.FieldRadius * 2 ))
                         pos = outPos;
                     break;
 
                 case GridMode.AroundBuildingCircle:
-                    if (!IsPointInRange( gamePos, pos, mouseRadius ))
+                    if (!IsPointInRange( gamePos, pos, GridConfig.FieldRadius ))
                         pos = outPos;
                     break;
 
@@ -333,10 +343,10 @@ function UpdateVisibleGrid()
         if (!altPressed)
         {
             altPressed = true;
-            gridMode++;
+            GridConfig.GridMode++;
 
-            if (gridMode > GridMode.OnlyFree)
-                gridMode = GridMode.None;
+            if (GridConfig.GridMode > GridMode.OnlyFree)
+                GridConfig.GridMode = GridMode.None;
         }
     }
     else
@@ -345,11 +355,20 @@ function UpdateVisibleGrid()
     $.Schedule(1/20, UpdateVisibleGrid);
 }
 
-function StartBuildingHelper( params )
+//-----------------------------------------------------------------------------
+//                         Entity particle vars
+//-----------------------------------------------------------------------------
+var size = 0;
+var particle;
+var gridParticles;
+
+//-----------------------------------------------------------------------------
+//                         Entity particle
+//-----------------------------------------------------------------------------
+function CreateEntityParticle( params )
 {
     if (params !== undefined)
-    {
-        state = params["state"];
+    {	
         size = params["size"];
 
         var scale = params["fMaxScale"]
@@ -366,8 +385,6 @@ function StartBuildingHelper( params )
             }
         }
 
-        $("#BuildingHelperBase").hittest = true;
-
         // Building Ghost
         particle = Particles.CreateParticle("particles/buildinghelper/ghost_model.vpcf", ParticleAttachment_t.PATTACH_ABSORIGIN, localHeroIndex);
         Particles.SetParticleControlEnt(particle, 1, entindex, ParticleAttachment_t.PATTACH_ABSORIGIN_FOLLOW, "follow_origin", Entities.GetAbsOrigin(entindex), true)
@@ -382,112 +399,141 @@ function StartBuildingHelper( params )
             var gridParticle = Particles.CreateParticle("particles/buildinghelper/square_sprite_building.vpcf", ParticleAttachment_t.PATTACH_CUSTOMORIGIN, 0)
             Particles.SetParticleControl(gridParticle, 1, [32,0,0])
             Particles.SetParticleControl(gridParticle, 2, [255,255,255]) //Keep the original color
-            Particles.SetParticleControl(gridParticle, 3, [GRID_ALPHA,0,0])
+            Particles.SetParticleControl(gridParticle, 3, [GridConfig.EntityGridAlpha,0,0])
             gridParticles.push(gridParticle)
         }
     } 
-    
-    if (state == 'active')
-    {
-        CheckMousePos();
-
-        BHPanel.Resolution = GetScreenResolution();
-        CreateVisibleGridParticle();
-        UpdateVisibleGrid();
-    }
 }
 
-function CheckMousePos()
+function UpdateEntityParticle()
 {
-    if (state == 'active')
+    if (state != 'active')
+        return;
+
+    var mousePos = GameUI.GetCursorPosition();
+    var GamePos = Game.ScreenXYToWorld(mousePos[0], mousePos[1]);
+
+    if (GamePos[0] > 10000000) // fix for borderless windowed players
+      GamePos = [0,0,0];
+
+    if ( GamePos !== null ) 
     {
-        var mousePos = GameUI.GetCursorPosition();
-        var GamePos = Game.ScreenXYToWorld(mousePos[0], mousePos[1]);
+        SnapToGrid(GamePos, size)
 
-        if (GamePos[0] > 10000000) // fix for borderless windowed players
-          GamePos = [0,0,0];
+        var color = [0,255,0]
+        var halfSide = (size/2)*64
+        var part = 0;
+        var boundingRect = {}
+        boundingRect["leftBorderX"] = GamePos[0]-halfSide
+        boundingRect["rightBorderX"] = GamePos[0]+halfSide
+        boundingRect["topBorderY"] = GamePos[1]+halfSide
+        boundingRect["bottomBorderY"] = GamePos[1]-halfSide
 
-        if ( GamePos !== null ) 
+        var isAnyQuadBlocked = false;
+        for (var x=boundingRect["leftBorderX"]+32; x <= boundingRect["rightBorderX"]-32; x+=64)
         {
-            SnapToGrid(GamePos, size)
-
-            var color = [0,255,0]
-            var halfSide = (size/2)*64
-            var part = 0;
-            var boundingRect = {}
-            boundingRect["leftBorderX"] = GamePos[0]-halfSide
-            boundingRect["rightBorderX"] = GamePos[0]+halfSide
-            boundingRect["topBorderY"] = GamePos[1]+halfSide
-            boundingRect["bottomBorderY"] = GamePos[1]-halfSide
-
-            var isAnyQuadBlocked = false;
-            for (var x=boundingRect["leftBorderX"]+32; x <= boundingRect["rightBorderX"]-32; x+=64)
+            for (var y=boundingRect["topBorderY"]-32; y >= boundingRect["bottomBorderY"]+32; y-=64)
             {
-                for (var y=boundingRect["topBorderY"]-32; y >= boundingRect["bottomBorderY"]+32; y-=64)
-                {
-                    var pos = [x,y,GamePos[2]]
-                    var gridParticle = gridParticles[part]
+                var pos = [x,y,GamePos[2]]
+                var gridParticle = gridParticles[part]
 
-                    var status = GetQuadStatus(pos);                    
-                    isAnyQuadBlocked = isAnyQuadBlocked || status == QuadStatus.Blocked;
+                var status = GetQuadStatus(pos);                    
+                isAnyQuadBlocked = isAnyQuadBlocked || status == QuadStatus.Blocked;
 
-                    Particles.SetParticleControl(gridParticle, 0, pos);
-                    Particles.SetParticleControl(gridParticle, 2,  GetBuildingQuadColor(status));
+                Particles.SetParticleControl(gridParticle, 0, pos);
+                Particles.SetParticleControl(gridParticle, 2,  GetBuildingQuadColor(status));
 
-                    part ++;
+                part ++;
 
-                    if (part > size*size)
-                        return;
-                }
+                if (part > size*size)
+                    return;
             }
-
-            // Update the model particle
-            Particles.SetParticleControl(particle, 0, [GamePos[0], GamePos[1], GamePos[2] + 1])
-            Particles.SetParticleControl(particle, 2, GetBuildingQuadColor( isAnyQuadBlocked ? QuadStatus.Blocked : QuadStatus.Free ))
         }
 
-        $.Schedule(1/60, CheckMousePos);    
+        // Update the model particle
+        Particles.SetParticleControl(particle, 0, [GamePos[0], GamePos[1], GamePos[2] + 1])
+        Particles.SetParticleControl(particle, 2, GetBuildingQuadColor( isAnyQuadBlocked ? QuadStatus.Blocked : QuadStatus.Free ))
     }
+
+    $.Schedule(1/60, UpdateEntityParticle);
 }
 
-function SendBuildCommand( params )
+function DestroyEntityParticle()
 {
-  var mPos = GameUI.GetCursorPosition();
-  var GamePos = Game.ScreenXYToWorld(mPos[0], mPos[1]);
-  GameEvents.SendCustomGameEventToServer( "building_helper_build_command", { "X" : GamePos[0], "Y" : GamePos[1], "Z" : GamePos[2] } );
-
-  Cancel(params);
-}
-
-function SendCancelCommand( params )
-{
-  Cancel();
-  GameEvents.SendCustomGameEventToServer( "building_helper_cancel_command", {} );
-}
-
-function Cancel() {
-    state = 'disabled'
-    Particles.DestroyParticleEffect(particle, true)
-    for (var i in gridParticles) {
+	if (particle)
+    	Particles.DestroyParticleEffect(particle, true)
+    for (var i in gridParticles)
         Particles.DestroyParticleEffect(gridParticles[i], true)
-    }
-
-    DestroyVisibleGridParticles();
-
-    $("#BuildingHelperBase").hittest = false;
 }
 
+//-----------------------------------------------------------------------------
+//                         GNV particle controls
+//-----------------------------------------------------------------------------
+function CreateParticles( params )
+{
+	state = params["state"];
+
+	CreateEntityParticle( params )
+	UpdateEntityParticle();
+
+    GNVPanel.Resolution = GetScreenResolution();
+    CreateVisibleGridParticle();
+    UpdateVisibleGrid();	
+}
+
+function DestroyParticles()
+{
+    state = 'disabled';
+
+	DestroyEntityParticle();
+	DestroyVisibleGridParticles();
+}
+
+//-----------------------------------------------------------------------------
+//                         Layers
+//-----------------------------------------------------------------------------
+function CreateLayer( layerName )
+{
+    GNVPanel.Grid[layerName] = [];
+    for (var j = 0; j < -GNVPanel.YMin + GNVPanel.YMax + 1; j++)
+    {
+        GNVPanel.Grid[layerName][j] = []
+        for (var i = 0; i < -GNVPanel.XMin + GNVPanel.XMax + 1; i++)
+            GNVPanel.Grid[layerName][j][i] = 0;
+    }    
+}
+
+function LayerChanged( table_name, key, data )
+{
+    if (GNVPanel.LastQueueNum >= parseInt(key, 10))
+        return
+
+    var layerName = data["LayerName"];
+    if (GNVPanel.Grid[layerName] == null)
+        CreateLayer( layerName );
+
+    var ltX = -GNVPanel.XMin + data["X"] - 1;
+    var ltY = -GNVPanel.YMin + data["Y"] - 1;
+
+    for (var y = 1; y <= data["Height"]; y++)
+        for (var x = 1; x <= data["Width"]; x++)
+            GNVPanel.Grid[layerName][ltY + y - 1][ltX + x - 1] = data['Mapping'][y][x];
+}
+
+//-----------------------------------------------------------------------------
+//                         GNV load/unpack
+//-----------------------------------------------------------------------------
 // Receive and decoding GNV
 function GNV( args )
 {
     $.Msg("GNV received")
 
-    BHPanel.XMin = args["XMin"];
-    BHPanel.XMax = args["XMax"];
-    BHPanel.YMin = args["YMin"];
-    BHPanel.YMax = args["YMax"];
+    GNVPanel.XMin = args["XMin"];
+    GNVPanel.XMax = args["XMax"];
+    GNVPanel.YMin = args["YMin"];
+    GNVPanel.YMax = args["YMax"];
 
-    $.Msg("XMin: ", BHPanel.XMin, " XMax: ", BHPanel.XMax, " YMin: ", BHPanel.YMin, " YMax: ", BHPanel.YMax)
+    $.Msg("XMin: ", GNVPanel.XMin, " XMax: ", GNVPanel.XMax, " YMin: ", GNVPanel.YMin, " YMax: ", GNVPanel.YMax)
 
     var decoded = ""
     var pad = "00000000"
@@ -529,64 +575,25 @@ function GNV( args )
     }
 
     var layerName = args["LayerName"]
-    BHPanel.Grid[layerName] = []
+    GNVPanel.Grid[layerName] = []
     var curQuad = 0;
-    for (var j = 0; j < -BHPanel.YMin + BHPanel.YMax + 1; j++)
+    for (var j = 0; j < -GNVPanel.YMin + GNVPanel.YMax + 1; j++)
     {
-        BHPanel.Grid[layerName][j] = []
-        for (var i = 0; i < -BHPanel.XMin + BHPanel.XMax + 1; i++)
-            BHPanel.Grid[layerName][j][i] = decoded[curQuad++];
+        GNVPanel.Grid[layerName][j] = []
+        for (var i = 0; i < -GNVPanel.XMin + GNVPanel.XMax + 1; i++)
+            GNVPanel.Grid[layerName][j][i] = decoded[curQuad++];
     }
 }
 
 //-----------------------------------------------------------------------------
-//                         Layers
+//                         Util functions
 //-----------------------------------------------------------------------------
-function CreateLayer( layerName )
-{
-    BHPanel.Grid[layerName] = [];
-    for (var j = 0; j < -BHPanel.YMin + BHPanel.YMax + 1; j++)
-    {
-        BHPanel.Grid[layerName][j] = []
-        for (var i = 0; i < -BHPanel.XMin + BHPanel.XMax + 1; i++)
-            BHPanel.Grid[layerName][j][i] = 0;
-    }    
-}
-
-function LayerChanged( table_name, key, data )
-{
-    if (BHPanel.LastQueueNum >= parseInt(key, 10))
-        return
-
-    var layerName = data["LayerName"];
-    if (BHPanel.Grid[layerName] == null)
-        CreateLayer( layerName );
-
-    var ltX = -BHPanel.XMin + data["X"] - 1;
-    var ltY = -BHPanel.YMin + data["Y"] - 1;
-
-    for (var y = 1; y <= data["Height"]; y++)
-        for (var x = 1; x <= data["Width"]; x++)
-            BHPanel.Grid[layerName][ltY + y - 1][ltX + x - 1] = data['Mapping'][y][x];
-}
-
-(function () {
-    GameEvents.SendCustomGameEventToServer( "gnv_request", { "Layers" : { } } );
-
-    GameEvents.Subscribe( "building_helper_enable", StartBuildingHelper);
-    GameEvents.Subscribe( "building_helper_force_cancel", Cancel);
-
-    CustomNetTables.SubscribeNetTableListener( "LayersQueue", LayerChanged );
-    GameEvents.Subscribe( "gnv", GNV);
-})();
-
-
 function CheckGridSquare( layerName, x, y)
 {
-    if (BHPanel.Grid[layerName])
-        if (BHPanel.Grid[layerName][y])
-            if (BHPanel.Grid[layerName][y][x])
-                return BHPanel.Grid[layerName][y][x] == 1;
+    if (GNVPanel.Grid[layerName])
+        if (GNVPanel.Grid[layerName][y])
+            if (GNVPanel.Grid[layerName][y][x])
+                return GNVPanel.Grid[layerName][y][x] == 1;
 
     return false;
 }
@@ -605,7 +612,7 @@ function IsBlocked(position) {
 
 function GetGridPosition( position )
 {
-    return [WorldToGridPosX(position[0]) - BHPanel.XMin, WorldToGridPosY(position[1]) - BHPanel.YMin];
+    return [WorldToGridPosX(position[0]) - GNVPanel.XMin, WorldToGridPosY(position[1]) - GNVPanel.YMin];
 }
 
 function SnapToGrid(vec, size) {
