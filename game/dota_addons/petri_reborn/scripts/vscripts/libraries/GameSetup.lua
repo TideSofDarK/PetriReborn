121830bbd2dd2442ca50bc04371dab0439f1cf49
K1llMan <k1llmanws@yandex.ru>
2016-02-18 21:32:16 +0300

    Remove debug prints

--- game/dota_addons/petri_reborn/scripts/vscripts/libraries/GameSetup.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/libraries/GameSetup.lua
@@ -72,11 +72,6 @@ function GetTeamsFromEmptySelection( args )
   local petr = args["petr"];
   local kvn = args["kvn"];
 
-  print("Initial tables")
-  DeepPrintTable(petr)
-  DeepPrintTable(kvn)
-  DeepPrintTable(GameSetup.votes['prefer_team'])
-
   -- Get min petr in game
   local minPetrCount = 0
   if GetArraySize( GameSetup.votes['prefer_team'] ) > 6 then
@@ -95,10 +90,6 @@ function GetTeamsFromEmptySelection( args )
           table.insert(kvn, k)
         end
       end
-
-  print("Prefers team")
-  DeepPrintTable(petr)
-  DeepPrintTable(kvn)
   
       -- Set random kvn to petr team if only host prefer this team
       if GetArraySize( petr ) < minPetrCount then
@@ -107,10 +98,6 @@ function GetTeamsFromEmptySelection( args )
       end
     end
   end
-
-  print("Final tables")
-  DeepPrintTable(petr)
-  DeepPrintTable(kvn)
   
   return petr, kvn  
 end
