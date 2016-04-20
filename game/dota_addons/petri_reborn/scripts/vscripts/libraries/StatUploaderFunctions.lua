4ece696eb63e8e397fe9c5145f5976c64a517791
TideSofDarK <tidesmain@gmail.com>
2016-04-20 19:21:14 +0500

    Temp prints

--- game/dota_addons/petri_reborn/scripts/vscripts/libraries/StatUploaderFunctions.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/libraries/StatUploaderFunctions.lua
@@ -52,9 +52,10 @@ end
 function SU:SavePlayersStats()
   local time = math.ceil(GameMode.PETRI_TRUE_TIME / 60)
   if time < 9 or time > 50  then --or not GameMode.villians["npc_dota_hero_death_prophet"]
+    print("sdasdasdsassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
     return
   end
-
+  print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
   local requestParams = {
     Command = "SavePlayersStats",
     PlayersStats = SU:BuildMMRArray()
