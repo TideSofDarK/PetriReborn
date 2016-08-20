ca9bf697648d9a735e2ddfb557014fa42b17225c
TideSofDarK <tidesmain@gmail.com>
2016-08-20 11:47:08 +0500

    Testing portals

--- game/dota_addons/petri_reborn/scripts/vscripts/triggers/portal.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/triggers/portal.lua
@@ -1,4 +1,5 @@
 function OnStartTouch(trigger)
+	print(string.match(trigger.caller:GetName(),"%d+"))
 	if string.match(trigger.caller:GetName(), string.gsub(trigger.activator:GetUnitName(), "npc_dota_hero_", "")) and (string.match(trigger.caller:GetName(),"%d+") and trigger.activator:GetLevel() >= tonumber(string.match(trigger.caller:GetName(),"%d+"))) then
 		local newPosition = thisEntity:GetAbsOrigin()
 
