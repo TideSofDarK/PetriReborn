db62be6e904691ee3cc7137576cc1daa85f84bf2
TideSofDarK <tidesmain@gmail.com>
2016-09-20 17:19:32 +0500

    Dependency stuff

--- game/dota_addons/petri_reborn/scripts/vscripts/libraries/dependencies.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/libraries/dependencies.lua
@@ -10,10 +10,40 @@ function CheckBuildingDependencies(pID, building)
 	
 	for k,v in pairs(GameMode.DependenciesKVs[building]) do
 		if CheckDependency(pID, k, v) == false then
-			if allow == true then Notifications:Top(pID,{text="#depedency_is_needed", duration=1, style={color="red"}, continue=false}) end
-			Notifications:Top(pID,{text="#DOTA_Tooltip_ability_"..k, duration=1, style={color="red"}, continue=false})
-			Notifications:Top(pID,{text=" ("..tostring(v)..")", duration=1, style={color="red"}, continue=true})
 			allow = false
+		end
+	end
+
+	if allow == false and GameMode.DependenciesKVs[building.."_alt"] then
+		allow = true
+		for k,v in pairs(GameMode.DependenciesKVs[building.."_alt"]) do
+			if CheckDependency(pID, k, v) == false then
+				allow = false
+			end
+		end
+	end
+
+	if allow == false then
+		local allow = true
+		for k,v in pairs(GameMode.DependenciesKVs[building]) do
+			if CheckDependency(pID, k, v) == false then
+				if allow == true then Notifications:Top(pID,{text="#depedency_is_needed", duration=1, style={color="red"}, continue=false}) end
+				Notifications:Top(pID,{text="#DOTA_Tooltip_ability_"..k, duration=1, style={color="red"}, continue=false})
+				Notifications:Top(pID,{text=" ("..tostring(v)..")", duration=1, style={color="red"}, continue=true})
+				allow = false
+			end
+		end
+
+		if allow == false and GameMode.DependenciesKVs[building.."_alt"] then
+			allow = true
+			for k,v in pairs(GameMode.DependenciesKVs[building.."_alt"]) do
+				if CheckDependency(pID, k, v) == false then
+					if allow == true then Notifications:Top(pID,{text="#depedency_is_needed", duration=1, style={color="red"}, continue=false}) end
+					Notifications:Top(pID,{text="#DOTA_Tooltip_ability_"..k, duration=1, style={color="red"}, continue=false})
+					Notifications:Top(pID,{text=" ("..tostring(v)..")", duration=1, style={color="red"}, continue=true})
+					allow = false
+				end
+			end
 		end
 	end
 
