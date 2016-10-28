1262a671a1eca12c6aa808995655cb3bf6b58e5c
AlikVirg <AlikVirg@gmail.com>
2016-10-28 18:22:14 +0300

    alikdayn

--- game/dota_addons/petri_reborn/scripts/vscripts/gamemode.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/gamemode.lua
@@ -640,3 +640,30 @@ function PetrosyanWin()
     end
   end
 end
+
+if Wearables == nil then
+    _G.Wearables = class({})
+end
+
+function Wearables:AttachWearable(unit, modelPath)
+    local wearable = SpawnEntityFromTableSynchronous("prop_dynamic", {model = modelPath, DefaultAnim=animation, targetname=DoUniqueString("prop_dynamic")})
+
+    wearable:FollowEntity(unit, true)
+
+    unit.wearables = unit.wearables or {}
+    table.insert(unit.wearables, wearable)
+
+    return wearable
+end
+
+function Wearables:Remove(unit)
+    if not unit.wearables or #unit.wearables == 0 then
+        return
+    end
+
+    for _, part in pairs(unit.wearables) do
+        part:RemoveSelf()
+    end
+
+    unit.wearables = {}
+end
