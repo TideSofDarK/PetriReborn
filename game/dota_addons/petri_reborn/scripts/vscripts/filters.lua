b5c822d9a3adc081b85fcddf114ec217e84131ba
TideSofDarK <tidesmain@gmail.com>
2016-10-28 21:01:16 +0500

    dfgfdgdfg

--- game/dota_addons/petri_reborn/scripts/vscripts/filters.lua
+++ game/dota_addons/petri_reborn/scripts/vscripts/filters.lua
@@ -176,7 +176,7 @@ function GameMode:FilterExecuteOrder( filterTable )
       local item = EntIndexToHScript(filterTable["entindex_target"]):GetContainedItem()
 
       local purchaser = EntIndexToHScript(units["0"])
-
+      if item:GetName() == "item_petri_candy" then return true end
       if check2(purchaser) and check1(item:GetName()) then
         return false
       end
